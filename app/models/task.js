6399a08bf3febadf5eae95c144ba7b20369232e5
wuyang-liu <liuwuyayui@gmail.com>
2022-04-05 16:38:13 -0700

    feat: first commit

--- app/models/task.js
+++ app/models/task.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const Task = new Schema({
-    id: { type: Number, unique: true, sparse: true },
-    name: { type: String, required: true, max: 60 },
-    desc: { type: String, required: true, max: 255 }
-});
-
-module.exports = mongoose.model('Task', Task);
+// const mongoose = require('mongoose');
+// const { Schema } = mongoose;
+//
+// const Task = new Schema({
+//     id: { type: Number, unique: true, sparse: true },
+//     name: { type: String, required: true, max: 60 },
+//     desc: { type: String, required: true, max: 255 }
+// });
+//
+// module.exports = mongoose.model('Task', Task);
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 
 require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const { port, mongoUrl } = require('./app/config');
 
 const app = express();
@@ -12,8 +13,12 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(require('./app/routes'));
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
-const db = mongoose.connection;
-db.on('error', (err) => console.error('MongoDB connection error:', err));
-db.once('open', () => app.listen(port, () => console.log(`Listening on port: ${port}`)));
+// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+
+// const db = mongoose.connection;
+// db.on('error', (err) => console.error('MongoDB connection error:', err));
+// db.once('open', () => app.listen(port, () => console.log(`Listening on port: ${port}`)));
