95c832ceb334eaa20e655a366e846bd1b915d3b3
wuyang-liu <liuwuyayui@gmail.com>
2022-04-05 22:00:28 -0700

    fix: fix package.json

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 router.get('/', (req, res) => res.sendFile('index.html'));
+router.get('/test', (req, res) => res.send('asdasdsd'));
 
 router.use('/entries', require('./entries'));
 
